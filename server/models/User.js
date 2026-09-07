f70827e1f865b679e8d8db4ac13ff071993160ac
Akshaya <akshaya.s92@kalvium.community>
2026-09-07 14:38:34 +0530

    Implement username password authentication

--- server/models/User.js
+++ server/models/User.js
@@ -2,11 +2,24 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
     name: {
       type: String,
       required: true,
       trim: true,
     },
+
     email: {
       type: String,
       required: true,
@@ -14,10 +27,12 @@ const userSchema = new mongoose.Schema(
       trim: true,
       lowercase: true,
     },
+
     phone: {
       type: String,
       required: true,
     },
+
     role: {
       type: String,
       enum: ["donor", "recipient", "admin"],
